9d25547d88a5e86f9fe10a7fd283218daf8759b2
Samuel <samuel@Samuels-MacBook-Pro.local>
2021-01-09 15:05:55 -0800

    finished adding rows using bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
 // date 
 var day = moment(new Date());
 $("#currentDay").html(day.format('dddd, MMMM Do'));
+var elements  = {};
+
+if (!elements) {
+    events = {
+      nine: [],
+      ten: [],
+      eleven: [],
+      twelve: [],
+      one: [],
+      two: [],
+      three: [],
+      four: [],
+      five: []
+
+    };
+  }
